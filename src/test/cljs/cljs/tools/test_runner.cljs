8b7be0912fdb362cbd9b205483ea979d4761692a
David Kettering <david.kettering@navy.mil>
2020-11-16 17:41:48 -0600

    Add failing peak-equals-read property test

--- src/test/cljs/cljs/tools/test_runner.cljs
+++ src/test/cljs/cljs/tools/test_runner.cljs
@@ -2,6 +2,7 @@
   (:require cljs.tools.metadata-test
             cljs.tools.reader-test
             cljs.tools.reader-edn-test
+            cljs.tools.reader-types-test
             [cljs.test :refer-macros [run-all-tests]]))
 
 (enable-console-print!)
