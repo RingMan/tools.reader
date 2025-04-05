c6bb271fbab4cb9ec15169c600efab4d145f239c
David Kettering <david.ringman@gmail.com>
2025-04-05 00:01:32 -0500

    Add back tests with #inst

--- src/test/clojure/clojure/tools/reader_edn_test.clj
+++ src/test/clojure/clojure/tools/reader_edn_test.clj
@@ -17,10 +17,10 @@
   (is (instance? clojure.lang.Keyword (read-string ":alphabet"))) )
 
 (deftest read-tagged
-  ;; (is (= #inst "2010-11-12T13:14:15.666"
-  ;;        (read-string "#inst \"2010-11-12T13:14:15.666\"")))
-  ;; (is (= #inst "2010-11-12T13:14:15.666"
-  ;;        (read-string "#inst\"2010-11-12T13:14:15.666\"")))
+  (is (= #inst "2010-11-12T13:14:15.666"
+         (read-string "#inst \"2010-11-12T13:14:15.666\"")))
+  (is (= #inst "2010-11-12T13:14:15.666"
+         (read-string "#inst\"2010-11-12T13:14:15.666\"")))
   ;; (is (= #uuid "550e8400-e29b-41d4-a716-446655440000"
   ;;        (read-string "#uuid \"550e8400-e29b-41d4-a716-446655440000\"")))
   ;; (is (= #uuid "550e8400-e29b-41d4-a716-446655440000"
